4668cae49712799518a5b6c4057410be5875e900
MKassabeh <49947266+MKassabeh@users.noreply.github.com>
2020-12-15 12:38:07 +0100

    sript random color

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
+//Déclaration Variable  
+
+var squarecolor =document.querySelectorAll(".square");
+var score = document.getElementById('colorDisplay');
+// Couleur aléatoire 
+
+
+function randomColor(){
+    var color = Math.floor(Math.random() * 255) + 1
+    return color
+}
 
 
 
+// Début du jeu 
 
-    
+score.textContent="RGB ( "+ r+" , "+g+" , "+b +" )" ;
 
 
+for (let index = 0; index < squarecolor.length; index++) {
 
+    var r=randomColor()
+    var g=randomColor()
+    var b=randomColor()
+    score.textContent="RGB ( "+ r+" , "+g+" , "+b +" )" ;
+    squarecolor[index].style.backgroundColor='rgb(' + r + ' ,' + g +','+ b  +')'
+
+}
