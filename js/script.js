6f6e2cbf4b985fd4833080ab0629f47b23b73c1f
MKassabeh <49947266+MKassabeh@users.noreply.github.com>
2020-12-15 13:52:21 +0100

    reset score button + visual modification

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 var squarecolor =document.querySelectorAll(".square");
 var score = document.getElementById('colorDisplay');
+var resetColor = document.getElementById("reset");
+var titleman=document.getElementById("mainTitle");
+var winningmessage=document.getElementById("message")
 // Couleur aléatoire 
 
 
@@ -10,11 +13,7 @@ function randomColor(){
     return color
 }
 
-
-
 // Début du jeu 
-
-score.textContent="RGB ( "+ r+" , "+g+" , "+b +" )" ;
 
 
 for (let index = 0; index < squarecolor.length; index++) {
@@ -22,7 +21,37 @@ for (let index = 0; index < squarecolor.length; index++) {
     var r=randomColor()
     var g=randomColor()
     var b=randomColor()
-    score.textContent="RGB ( "+ r+" , "+g+" , "+b +" )" ;
-    squarecolor[index].style.backgroundColor='rgb(' + r + ' ,' + g +','+ b  +')'
-
+    score.textContent='rgb('+r+', '+g+', '+b+')' ;
+    squarecolor[index].style.backgroundColor='rgb('+r+', '+g+', '+b+')';
+   
+    squarecolor[index].addEventListener('click',function(){
+        if(squarecolor[index].style.backgroundColor===score.textContent){
+            
+            squarecolor.forEach(element => {
+                winningmessage.textContent="Correct!"
+                resetColor.textContent="Play Again ?"
+                titleman.style.backgroundColor=score.textContent
+                element.style.visibility="visible";
+                element.style.backgroundColor=score.textContent
+                
+        })
+    }else{
+        squarecolor[index].style.visibility="hidden"
+    }
+    })
 }
+
+resetColor.addEventListener('click',function(){
+    squarecolor.forEach(element => {
+        resetColor.textContent="New Color"
+        titleman.style.backgroundColor="steelblue"
+        winningmessage.textContent="";
+        var r=randomColor()
+        var g=randomColor()
+        var b=randomColor()
+        element.style.backgroundColor='rgb('+r+', '+g+', '+b+')';
+        score.textContent='rgb('+r+', '+g+', '+b+')';
+    });
+    
+    
+})
