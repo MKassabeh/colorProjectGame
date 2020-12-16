8b6fbb94a1ab5b6842334fb4c4176ffde86da01e
MKassabeh <49947266+MKassabeh@users.noreply.github.com>
2020-12-16 13:22:48 +0100

    fin indentation

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ var resetColor = document.getElementById("reset");
 var titleman=document.getElementById("mainTitle");
 var winningmessage=document.getElementById("message");
 var easymode = document.getElementById("modeEasy");
-    var hardmode=document.getElementById("mode");
-    var answer = Math.floor(Math.random() * squarecolor.length)
+var hardmode=document.getElementById("mode");
+var answer = Math.floor(Math.random() * squarecolor.length)
 
 // Couleur aléatoire 
 
@@ -45,7 +45,6 @@ function easyMode(){
         }
     }
 }
-
 // Début du jeu 
 
 
@@ -62,58 +61,53 @@ for (let index = 0; index < squarecolor.length; index++) {
     }
         // click on square 
 
-        squarecolor[index].addEventListener('click',function(){
-            if(squarecolor[index].style.backgroundColor===score.textContent){
-                    squarecolor.forEach(element => {
-                        winningmessage.textContent="Correct!"
-                        resetColor.textContent="Play Again ?"
-                        titleman.style.backgroundColor=score.textContent
-                        element.style.visibility="visible";
-                        element.style.backgroundColor=score.textContent
-                    
-                    })
-            }else{
-            squarecolor[index].style.visibility="hidden"
-            winningmessage.textContent="Retry !"
-            }
-        })
+    squarecolor[index].addEventListener('click',function(){
+        if(squarecolor[index].style.backgroundColor===score.textContent){
+                squarecolor.forEach(element => {
+                    winningmessage.textContent="Correct!"
+                    resetColor.textContent="Play Again ?"
+                    titleman.style.backgroundColor=score.textContent
+                    element.style.visibility="visible";
+                    element.style.backgroundColor=score.textContent
+                
+                })
+        }else{
+        squarecolor[index].style.visibility="hidden"
+        winningmessage.textContent="Retry !"
+        }
+    })
 
 }
 
 // reset button
 resetColor.addEventListener('click',function(){
     
-        resetColor.textContent="New Color"
-        titleman.style.backgroundColor="steelblue"
-        winningmessage.textContent="";
-        squarecolor.forEach(element => {
-        if(easymode.className==="selected"){
-            easymode.classList.add('selected')
-            easyMode()
+    resetColor.textContent="New Color"
+    titleman.style.backgroundColor="steelblue"
+    winningmessage.textContent="";
+    if(easymode.className==="selected"){
+        easymode.classList.add('selected')
+        easyMode()
+        
+    }else{
+        easymode.classList.remove('selected')
+        var answer = Math.floor(Math.random() * squarecolor.length)
+        for (let index = 0; index < squarecolor.length; index++) {
+            var r=randomColor()
+            var g=randomColor()
+            var b=randomColor()
             
-        }
-        else{
-            easymode.classList.remove('selected')
-            var answer = Math.floor(Math.random() * squarecolor.length)
-            for (let index = 0; index < squarecolor.length; index++) {
-                var r=randomColor()
-                var g=randomColor()
-                var b=randomColor()
-               
-                if(answer===index){
-                score.textContent='rgb('+r+', '+g+', '+b+')'
-                squarecolor[answer].style.backgroundColor=score.textContent
-                console.log(answer)
+            if(answer===index){
+            score.textContent='rgb('+r+', '+g+', '+b+')'
+            squarecolor[answer].style.backgroundColor=score.textContent
+            console.log(answer)
             }else{
                 squarecolor[index].style.backgroundColor='rgb('+r+', '+g+', '+b+')';
             }  
-                
-            }
- 
             
-        }
-    });    
-})
+        } 
+    }
+});    
 
 
 // easyMode button
